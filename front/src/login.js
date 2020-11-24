995c0ead67f8d5965fc1e209f6a188b52c820e1a
Arthur Benard <abenard@localhost.localdomain>
2020-11-24 16:25:52 +0100

    [FIX] move const my_button from login.js and register.js to my_style_button.js

--- front/src/login.js
+++ front/src/login.js
@@ -12,25 +12,9 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { styled } from '@material-ui/core/styles';
+import MyButton from './my_style_button.js'
 import { useHistory } from 'react-router-dom';
 
-
-const MyButton = styled(({ color, ...other }) => <Button {...other} />)({
-  background: (props) =>
-    props.color === 'red'
-      ? 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
-      : 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
-  border: 0,
-  borderRadius: 3,
-  boxShadow: (props) =>
-    props.color === 'red'
-      ? '0 3px 5px 2px rgba(255, 105, 135, .3)'
-      : '0 3px 5px 2px rgba(33, 203, 243, .3)',
-  color: 'white',
-  height: 48,
-  padding: '0 30px',
-  margin: 8,
-});
 
 function Copyright() {
   return (
